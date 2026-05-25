cb47214a47a977303adc90f0db5444d0f35ffc66
Prokhor1337 <prokhor1337@gmail.com>
2026-05-25 20:16:36 +0000

    Make prac6 unique

--- prac6/practical6.js
+++ prac6/practical6.js
@@ -1,24 +1,23 @@
 "use strict";
 
-import { greet, add, sumAll } from "./utils.js";
-import { user } from "./data.js";
-import { x } from "./data.js";
-import { reverseNumber } from "./utils.js";
+import { welcomeMessage, multiply, calculateAverage, squareNumber } from "./utils.js";
+import { student, defaultMultiplier } from "./data.js";
 
-const { userName, age } = user;
+const { firstName, university } = student;
 
-console.log(greet(userName)); 
-console.log(`Вік користувача: ${age}`);
+console.log(welcomeMessage(firstName)); 
+console.log(`Навчальний заклад: ${university}`);
 
-const scores = [10, 20, 30];
-const moreScores = [...scores, 40, 50];
+const grades = [85, 90, 95];
+const allGrades = [...grades, 100, 88];
 
-console.log("Всі бали:", moreScores);
-console.log("Сума балів:", sumAll(...moreScores));
+console.log("Всі оцінки:", allGrades);
+console.log("Середній бал:", calculateAverage(...allGrades).toFixed(1));
 
 document.getElementById("app").innerHTML = `
-    <h2>Користувач: ${userName}</h2>
-    <p>Результат додавання 5 + 5 = ${add(5, 5)}</p>
+    <h2>Студент: ${firstName}</h2>
+    <p>Місце навчання: ${university}</p>
+    <hr>
+    <p>Результат множення 12 * 4 = ${multiply(12, 4)}</p>
+    <p>Квадрат числа ${defaultMultiplier} дорівнює ${squareNumber(defaultMultiplier)}</p>
 `;
-
-console.log(reverseNumber(x));
--- prac6/utils.js
+++ prac6/utils.js
@@ -1,15 +1,17 @@
 "use strict";
 
-export function greet(name) {
-    return `Привіт, ${name}!`; 
+export function welcomeMessage(name) {
+    return `Вітаємо на сторінці, ${name}!`; 
 }
 
-export const add = (a, b) => a + b; 
+export const multiply = (a, b) => a * b; 
 
-export function sumAll(...nums) { 
-    return nums.reduce((acc, num) => acc + num, 0);
+export function calculateAverage(...numbers) { 
+    if (numbers.length === 0) return 0;
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    return sum / numbers.length;
 }
 
-export function reverseNumber(num){
-    return num*(-1);
+export function squareNumber(num) {
+    return num * num;
 }
