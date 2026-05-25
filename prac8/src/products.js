9e535cf88584539c9bf639dd12ae6d14a9e10784
Prokhor1337 <prokhor1337@gmail.com>
2026-05-25 20:12:06 +0000

    Fix UI issues

--- prac8/src/products.js
+++ prac8/src/products.js
@@ -1,5 +1,5 @@
 export const products = [
-    { id: 1, title: "Пляшка", price: 100, img: "https://via.placeholder.com/150" },
-    { id: 2, title: "Рюкзак", price: 500, img: "https://via.placeholder.com/150" },
-    { id: 3, title: "Ліхтарик", price: 200, img: "https://via.placeholder.com/150" }
+    { id: 1, title: "Пляшка", price: 100, img: "https://placehold.co/150x150/555/FFF?text=Bottle" },
+    { id: 2, title: "Рюкзак", price: 500, img: "https://placehold.co/150x150/555/FFF?text=Backpack" },
+    { id: 3, title: "Ліхтарик", price: 200, img: "https://placehold.co/150x150/555/FFF?text=Flashlight" }
 ];
